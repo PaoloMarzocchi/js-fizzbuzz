9789b446ffe866f56e2483a33e5b0310bfb6059c
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-21 16:50:19 +0100

    add the for loop, check remainders and print output in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
+const numbersElement = document.getElementById('numbers');
+//console.log(numbersElement);
 
+for (let i=1; i <= 100; i++){
+    //console.log(i);
+    let remainder3 = i % 3;
+    let remainder5 = i % 5;
 
+    if (remainder3 == 0 && remainder5 == 0) {
+        console.log('FizzBuzz');
+    } else if (remainder3 == 0) {
+        console.log('Fizz');
+    } else if (remainder5 == 0 ) {
+        console.log('Buzz');
+    } else{
+        console.log(i);
+    }
+    
+}
