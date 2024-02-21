7be3b35f7c134210fcf3a5cbdd947d8ebd98c6db
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-21 17:23:15 +0100

    add output printed in html file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,31 @@
 const numbersElement = document.getElementById('numbers');
 //console.log(numbersElement);
 
-for (let i=1; i <= 100; i++){
+/* let remainder5 = i % 5;
+console.log(remainder5); */
+
+for (let i = 1; i <= 10; i++) {
     //console.log(i);
     let remainder3 = i % 3;
-    let remainder5 = i % 5;
+    console.log(remainder3);
+    numbersElement.insertAdjacentHTML('beforeend',`<li>${i}<li>`);
+    /*  if (remainder3 == 0 && remainder5 == 0) {
+        // console.log('FizzBuzz');
+        numbersElement.innerHTML = `<li>FizzBuzz<li>`
+        //numbersElement.insertAdjacentHTML('beforeend',`<li>FizzBuzz<li>`);
+     } else if (remainder3 == 0) {
+         // console.log('Fizz');
+         numbersElement.innerHTML = `<li>Fizz<li>`
+         //numbersElement.insertAdjacentHTML('beforeend',`<li>Fizz<li>`);
+     } else if (remainder5 == 0 ) {
+         //console.log('Buzz');
+         numbersElement.innerHTML = `<li>Buzz<li>`
+         //numbersElement.insertAdjacentHTML('beforeend',`<li>Buzz<li>`);
+     } else{
+         //console.log(i);
+         numbersElement.innerHTML = `<li>${i}<li>`
+         //numbersElement.insertAdjacentHTML('beforeend',`<li>${i}<li>`);
+     }
+      */
 
-    if (remainder3 == 0 && remainder5 == 0) {
-        console.log('FizzBuzz');
-    } else if (remainder3 == 0) {
-        console.log('Fizz');
-    } else if (remainder5 == 0 ) {
-        console.log('Buzz');
-    } else{
-        console.log(i);
-    }
-    
 }
